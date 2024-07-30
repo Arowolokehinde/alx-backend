2d9717eb2c2fde1773c20d03c8b665082b09c34f
Arowoolo kehinde <arowolokehinde321@gmail.com>
2024-07-30 18:01:48 +0100

    babel

---
+++ 0x02-i18n/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template
+from flask_babel import Babel, gettext as _
+
+def get_locale():
+  return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+app = Flask(__name__)
+
+
+class Config:
+  LANGUAGES = ["en", "fr"]
+  BABEL_DEFAULT_LOCALE = "en"
+  BABEL_DEFAULT_TIMEZONE = "UTC"
+  
+app.config.from_object(Config)
+
+babel = Babel(app, locale_selector=get_locale)
+
+@app.route('/')
+def index():
+  """ This route returns the index.html """
+  return render_template('1-index.html')
+
+if __name__ == '__main__':
+  app.run(debug=True)
